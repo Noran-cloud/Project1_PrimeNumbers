09f540d1d1ecd23de1af2620d91cb81517fbf71f
Noran-cloud <121050473+Noran-cloud@users.noreply.github.com>
2022-12-26 21:36:39 +0200

    Add files via upload

---
+++ prime numbers.cs
@@ -0,0 +1,25 @@
+﻿
+
+using System;
+public class Prime_numbers
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Please,enter first number ");
+      int Firstnumber=int.Parse(Console.ReadLine());
+      Console.WriteLine("Please,enter last number");
+     int Lastnumber=int.Parse(Console.ReadLine());
+     Console.WriteLine("Prime numbers between them are :");
+     int i;
+     for (; Firstnumber<Lastnumber;Firstnumber++)
+     {
+         for (i = 2; i<=Firstnumber; i++)
+         {
+         if (Firstnumber % i == 0)
+         break;
+     }
+        if (i==Firstnumber)
+        Console.Write(",{0}",i);
+     }
+    }
+}
